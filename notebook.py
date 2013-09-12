045bd53bfe3e1f82efa63fc00c66234ac85f5a58
fuqcool <fuqcool@gmail.com>
2013-09-12 15:46:50 -0400

    always create a new file to write log file name end with .txt save log on each step

--- notebook.py
+++ notebook.py
@@ -4,11 +4,14 @@ __author__ = 'Jilin Yan'
 
 import sys, time
 
-if __name__ == '__main__':
+def timestamp():
+    return time.strftime('%m/%d %H:%M', time.localtime(time.time()))
 
-    notebook = file("./notebook.log", "w+")
-    notebook_lines = ""
-    
+def loop(notebook):
+    def write_log(txt):
+        notebook.write(txt + "\n")
+        notebook.flush()
+
     while(True):
         print "--------------- start -question -comments or exit ---------------"
         input_op = raw_input().split()
@@ -20,19 +23,25 @@ if __name__ == '__main__':
             comments = input_op[2]
             for i in range (3, len(input_op)):
                 comments += " " + input_op[i]
-            output_line = "%s\tstart\t%d\t%s" % (time.strftime('%m/%d %H:%M', time.localtime(time.time())), question_num, comments)
-            notebook_lines += output_line + "\n"
+
+            write_log("%s\tstart\t%d\t%s" % (timestamp(), question_num, comments))
+
             while(True):
                 print "--------------- stop or pause or resume ---------------"
                 input_op = raw_input()
                 if input_op == "stop" or input_op == "pause" or input_op == "resume":
-                    output_line = "%s\t%s\t%d\t%s" % (time.strftime('%m/%d %H:%M', time.localtime(time.time())), input_op, question_num, comments)
-                    notebook_lines += output_line + "\n"
-                if input_op == "stop":
-                    notebook_lines += "--------------- Split Line ---------------\n"
-                    break
+                    write_log("%s\t%s\t%d\t%s" % (timestamp(), input_op, question_num, comments))
+                    if input_op == "stop":
+                        write_log("--------------- Split Line ---------------")
+                        break
+
         elif first_op == "exit":
-            notebook.writelines(notebook_lines)
-            notebook.flush()
-            notebook.close()
             exit(0)
+
+if __name__ == '__main__':
+    suffix = int(time.time())
+    with open("./notebook_%s.log" % suffix, "w") as notebook:
+        loop(notebook)
+
+
+
